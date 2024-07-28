f9e964b6386f4ce927663a9614c667aa518d5d84
이정규 <dkssud6115@gmail.com>
2024-07-28 16:12:13 +0900

    비난 쿠키 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,19 @@ import { GoogleAnalytics } from '@next/third-parties/google';
 const jua = Jua({ subsets: ['latin'], weight: '400' });
 
 export const metadata: Metadata = {
-	title: 'Binan Cookie',
+	title: 'Binan Cookie | 비난 쿠키',
 	description:
-		"Hearing good words is important, but don't you think sometimes a sharp remark is necessary to live one's life",
+		'언제나 좋은 말만 들으면서 살 수는 없잖아요? 비판을 통해 자신을 돌아보고 성장하는 계기를 만들어 봅시다.',
 	icons: {
 		icon: '/icon.ico',
 	},
+	alternates: {
+		canonical: '/',
+		languages: {
+			'ko-KR': '/ko-KR',
+		},
+	},
+	keywords: ['비난 쿠키', 'binan cookie', '회고', '질타', '비난', '교훈'],
 	openGraph: {
 		title: 'Binan Cookie',
 		description:
@@ -19,9 +26,7 @@ export const metadata: Metadata = {
 		url: 'https://binan-cookie.vercel.app',
 		images: [
 			{
-				url: '/sneer_cookie.png',
-				width: 300,
-				height: 200,
+				url: 'https://binan-cookie.vercel.app/sneer_cookie.png',
 				alt: 'Og Image Alt',
 			},
 		],
