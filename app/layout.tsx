52208e154c19ee80acf4f264fcfb9e6d91004ecc
이정규 <dkssud6115@gmail.com>
2024-07-28 15:58:05 +0900

    seo 설정 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,13 +15,13 @@ export const metadata: Metadata = {
 	openGraph: {
 		title: 'Binan Cookie',
 		description:
-			"Hearing good words is important, but don't you think sometimes a sharp remark is necessary to live one's life",
+			'언제나 좋은 말만 들으면서 살 수는 없잖아요? 비판을 통해 자신을 돌아보고 성장하는 계기를 만들어 봅시다.',
 		url: 'https://binan-cookie.vercel.app',
 		images: [
 			{
 				url: '/sneer_cookie.png',
-				width: 800,
-				height: 600,
+				width: 300,
+				height: 200,
 				alt: 'Og Image Alt',
 			},
 		],
