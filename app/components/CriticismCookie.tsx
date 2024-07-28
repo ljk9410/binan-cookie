504d13c29ec7dcee33ce1d58d0cda471da5f4204
이정규 <dkssud6115@gmail.com>
2024-07-28 14:45:52 +0900

    간단한 디자인 수정

--- app/components/CriticismCookie.tsx
+++ app/components/CriticismCookie.tsx
@@ -20,6 +20,12 @@ const CriticismCookie = ({ comments }: { comments: string[][] }) => {
 			: phase === 1
 			? 'sneer_cookie'
 			: 'broken_cookie';
+	const clickMessage =
+		phase === 0
+			? '⬆️ Click ⬆️'
+			: phase === 1
+			? '⬆️ Crush the Cookie! ⬆️'
+			: 'Retry';
 
 	const onSetRandomNum = () => {
 		setRandomNum(Math.floor(Math.random() * comments.length));
@@ -31,7 +37,7 @@ const CriticismCookie = ({ comments }: { comments: string[][] }) => {
 			setTimeout(() => {
 				setIsLoading(false);
 				setPhase(1);
-			}, 1000);
+			}, 800);
 		} else if (phase === 1) {
 			setPhase(2);
 		} else if (phase === 2) {
@@ -45,20 +51,24 @@ const CriticismCookie = ({ comments }: { comments: string[][] }) => {
 	}, []);
 
 	return (
-		<div className="flex flex-col items-center">
-			<p className={`text-[32px] mb-8`}>Binan Cookie</p>
-			<Image
-				src={cookieSrc}
-				width={300}
-				height={300}
-				alt={imgAlt}
-				className={`bg-transparent ${isLoading && 'animate-wiggle'}`}
-				onClick={onClickCookie}
-			/>
-			<div className="animate-bounce mt-4">
-				<p className="text-[20px]">{phase === 2 ? 'Retry' : '⬆️ Click ⬆️'}</p>
+		<div className="flex flex-col w-full items-center pb-36">
+			<p className={`text-[32px]`}>Binan Cookie</p>
+			<div className={'w-[250px] h-[300px] flex flex-col justify-end'}>
+				<Image
+					src={cookieSrc}
+					width={250}
+					height={250}
+					alt={imgAlt}
+					className={`bg-transparent ${isLoading && 'animate-wiggle'} ${
+						phase === 1 && 'animate-wiggleUpDown'
+					} ${phase === 2 && 'animate-fadeInDown'}`}
+					onClick={onClickCookie}
+				/>
 			</div>
-			<div className={`mt-4 text-[28px]`}>
+			<div className="animate-bounce mt-4">
+				<p className="text-[20px]">{clickMessage}</p>
+			</div>
+			<div className={`mt-4 text-[24px] w-[320px] py-2 px-4 text-center`}>
 				{phase === 0 ? <p></p> : <p>{comments[randomNum][phase - 1]}</p>}
 			</div>
 		</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,9 +18,19 @@ const config: Config = {
 					'0%, 100%': { transform: 'rotate(10deg)' },
 					'50%': { transform: 'rotate(-10deg)' },
 				},
+				wiggleUpDown: {
+					'0%, 100%': { transform: 'translateY(0.7%)' },
+					'50%': { transform: 'translateY(-0.7%)' },
+				},
+				fadeInDown: {
+					'0%': { opacity: '0', transform: 'translateY(-40px)' },
+					'100%': { opacity: '1', transform: 'translateY(0)' },
+				},
 			},
 			animation: {
 				wiggle: 'wiggle 0.5s ease-in-out infinite',
+				wiggleUpDown: 'wiggleUpDown 0.2s ease-in-out infinite',
+				fadeInDown: 'fadeInDown 0.8s ease-in-out forwards',
 			},
 		},
 	},
